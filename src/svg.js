76aba1cb79310c01728c2140cfec469c7a7878f5
Arthur Black <hellomynameisarthurblack@gmail.com>
2018-08-16 22:44:00 -0600

    Successfully render SVG horizontal, vertical, and diagonal lines. Huzzah.

--- src/svg.js
+++ src/svg.js
@@ -1,13 +1,34 @@
 import React from 'react';
 
-class SVG extends React.Component {
-  render() {
-    return (
-      <svg>
-        <circle cx={50} cy={50} r={10} fill="red" />
-      </svg>
-    );
-  }
-}
+const SVG = props => (
+  <svg>
+
+    <g stroke="black" >
+      <line
+        x1="0"
+        y1="10"
+        x2="800"
+        y2="10"
+        strokeWidth="1"
+      />
+
+      <line
+        x1="10"
+        y1="0"
+        x2="10"
+        y2="800"
+        strokeWidth="1"
+      />
+
+      <line
+        x1="0"
+        y1="0"
+        x2="800"
+        y2="800"
+        strokeWidth="1"
+      />
+    </g>
+  </svg>
+);
 
 export default SVG;
