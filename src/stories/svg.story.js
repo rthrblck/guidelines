7e1307b0fd42142db29b8786ec4ef69d1da2d312
Arthur Black <hellomynameisarthurblack@gmail.com>
2018-08-23 10:01:21 -0600

    Render single line using an array containing a line object with associated attributes. Use map method and spread operator to iterate over array and distribute key/value pairs as new line attributes.e

--- src/stories/svg.story.js
+++ src/stories/svg.story.js
@@ -4,15 +4,47 @@ import { storiesOf } from '@storybook/react';
 
 import SVG from '../svg';
 
+const lineArray = [
+  {
+    x1: '50',
+    y1: '0',
+    x2: '50',
+    y2: '1000',
+    strokeWidth: '1',
+    stroke: 'red',
+    transform: 'rotate(60,50,0)',
+  },
+];
+
 class MySVG extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // Fundamental lines
+      nibWidth: 3,
+      xHeight: 5,
+      aHeight: 5,
+      dHeight: 5,
+      cHeight: 4,
+      bHeight: 0,
+      iSpace: 1,
+      // Verticality and pen angle
+      vert: 60,
+      vertSpace: 10,
+      penAngle: 0,
+    };
+  }
+
   render() {
     return (
-      <SVG />
+      <SVG
+        lineArray={lineArray}
+      />
     );
   }
 }
 
 storiesOf('svg', module)
   .add('initial setup', () => (
-    <MySVG />
+    <SVG />
   ));
--- src/svg.js
+++ src/svg.js
@@ -1,116 +1,21 @@
 import React from 'react';
 
-const SVG = props => (
+const lineArray = [
+  {
+    x1: '50',
+    y1: '0',
+    x2: '50',
+    y2: '1000',
+    strokeWidth: '1',
+    stroke: 'red',
+    transform: 'rotate(60,50,0)',
+  },
+];
+
+const SVG = () => (
   <svg width="1000" height="1000">
-
-    <g stroke="black" >
-
-      {/* Horizontal lines */}
-
-      <line
-        x1="0"
-        y1="10"
-        x2="800"
-        y2="10"
-        strokeWidth="1"
-      />
-
-      <line
-        x1="0"
-        y1="15"
-        x2="800"
-        y2="15"
-        strokeDasharray="5,5"
-        strokeWidth="1"
-      />
-
-      <line
-        x1="0"
-        y1="20"
-        x2="800"
-        y2="20"
-        strokeWidth="1"
-      />
-
-      <line
-        x1="0"
-        y1="60"
-        x2="800"
-        y2="60"
-        strokeWidth="1"
-      />
-
-      <line
-        x1="0"
-        y1="70"
-        x2="800"
-        y2="70"
-        strokeWidth="1"
-      />
-
-      {/* Diagonal lines */}
-      <g stroke="red" >
-
-        <line
-          x1="50"
-          y1="0"
-          x2="50"
-          y2="1000"
-          strokeWidth="1"
-          transform="rotate(60,50,0)"
-        />
-
-        <line
-          x1="100"
-          y1="0"
-          x2="100"
-          y2="1000"
-          strokeWidth="1"
-          transform="rotate(60,100,0)"
-        />
-
-        <line
-          x1="150"
-          y1="0"
-          x2="150"
-          y2="1000"
-          strokeWidth="1"
-          transform="rotate(60,150,0)"
-        />
-
-      </g>
-
-      {/* Vertical lines */}
-      <g stroke="lightgray" >
-
-        <line
-          x1="50"
-          y1="0"
-          x2="50"
-          y2="800"
-          strokeWidth="1"
-        />
-
-        <line
-          x1="100"
-          y1="0"
-          x2="100"
-          y2="800"
-          strokeWidth="1"
-        />
-
-        <line
-          x1="150"
-          y1="0"
-          x2="150"
-          y2="800"
-          strokeWidth="1"
-        />
-
-      </g>
-
-    </g>
-
+    {lineArray.map(line =>
+      <line {...line} />)}
   </svg>
 );
 
