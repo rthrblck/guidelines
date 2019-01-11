7e21498de630be4d49e301aceeccdb0097d5d39b
Arthur Black <hellomynameisarthurblack@gmail.com>
2019-01-11 16:18:01 -0700

    Attempt to fix update changes that broke everything

--- src/svg.js
+++ src/svg.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const SVG = props => (
-  <svg width="100%" height="1000">
+  <svg width="215.9mm" height="279.4mm">
     {props.lineArray.map(line =>
       <line {...line} />)}
   </svg>
