b524e353eb95d46afaed302e941a60626212bde0
Arthur Black <hellomynameisarthurblack@gmail.com>
2019-01-17 17:01:18 -0700

    Hard coded fundamental guidelines

--- src/stories/svg.story.js
+++ src/stories/svg.story.js
@@ -4,35 +4,35 @@ import { storiesOf } from '@storybook/react';
 
 import SVG from '../svg';
 
-const lineArray = [
-  {
-    x1: '50',
-    y1: '0',
-    x2: '50',
-    y2: '1000',
-    strokeWidth: '1',
-    stroke: 'red',
-    transform: 'rotate(60,50,0)',
-  },
-  {
-    x1: '100',
-    y1: '0',
-    x2: '100',
-    y2: '1000',
-    strokeWidth: '1',
-    stroke: 'red',
-    transform: 'rotate(60,100,0)',
-  },
-  {
-    x1: '150',
-    y1: '0',
-    x2: '150',
-    y2: '1000',
-    strokeWidth: '1',
-    stroke: 'red',
-    transform: 'rotate(60,150,0)',
-  },
-];
+// const lineArray = [
+//   {
+//     x1: '50',
+//     y1: '0',
+//     x2: '50',
+//     y2: '1000',
+//     strokeWidth: '1',
+//     stroke: 'red',
+//     transform: 'rotate(60,50,0)',
+//   },
+//   {
+//     x1: '100',
+//     y1: '0',
+//     x2: '100',
+//     y2: '1000',
+//     strokeWidth: '1',
+//     stroke: 'red',
+//     transform: 'rotate(60,100,0)',
+//   },
+//   {
+//     x1: '150',
+//     y1: '0',
+//     x2: '150',
+//     y2: '1000',
+//     strokeWidth: '1',
+//     stroke: 'red',
+//     transform: 'rotate(60,150,0)',
+//   },
+// ];
 
 // Not sure how to structure this function for clean prop passing. Maybe it needs a `this` binding?
 // lineArray.map(line =>
@@ -52,7 +52,5 @@ class MySVG extends React.Component {
 
 storiesOf('svg', module)
   .add('initial setup', () => (
-    <SVG
-      lineArray={lineArray}
-    />
+    <SVG />
   ));
--- src/svg.js
+++ src/svg.js
@@ -2,9 +2,42 @@ import React from 'react';
 
 const SVG = props => (
   <svg width="100%" height="1000">
-    {props.lineArray.map(line =>
-      <line {...line} />)}
+    <line
+      x1="0"
+      y1="10"
+      x2="1000"
+      y2="10"
+      strokeWidth="1"
+      stroke="black"
+    />
+    <line
+      x1="0"
+      y1="30"
+      x2="1000"
+      y2="30"
+      strokeWidth="1"
+      stroke="black"
+    />
+    <line
+      x1="0"
+      y1="70"
+      x2="1000"
+      y2="70"
+      strokeWidth="1"
+      stroke="black"
+    />
+    <line
+      x1="0"
+      y1="90"
+      x2="1000"
+      y2="90"
+      strokeWidth="1"
+      stroke="black"
+    />
   </svg>
 );
 
 export default SVG;
+
+// {props.lineArray.map(line =>
+//   <line {...line} />)}
