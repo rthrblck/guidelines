c3524aee4742eaa479dfef8f66c1c6a8c7648042
Arthur Black <hellomynameisarthurblack@gmail.com>
2019-01-27 17:16:47 -0700

    Return new drawline function and test to default config

--- src/jest/drawLine.test.js
+++ src/jest/drawLine.test.js
@@ -1,12 +1,13 @@
 const drawLine = require('./drawLine');
 
-test('generates a line with default values', () => {
-  expect(drawLine()).toEqual({
-    x1: '0',
-    y1: '10',
-    x2: '1000',
-    y2: '10',
-    strokeWidth: '1',
-    stroke: 'black',
-  });
+test('generates an object with default key/values', () => {
+  expect(drawLine()).toEqual(
+    {
+      x1: '0',
+      y1: '10',
+      x2: '1000',
+      y2: '10',
+      strokeWidth: '1',
+      stroke: 'black',
+    });
 });
