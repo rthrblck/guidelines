4dba073f707cb12eefdea29ff4ab658295e04707
Arthur Black <hellomynameisarthurblack@gmail.com>
2019-02-10 10:31:05 -0700

    Update npm

--- src/jest/drawLine.js
+++ src/jest/drawLine.js
@@ -1,4 +1,4 @@
-function drawLine(x1 = '0', y1 = '10', x2 = '1000', y2 = '10', w = '1', s = 'black') {
+function line(x1 = '0', y1 = '10', x2 = '1000', y2 = '10', w = '1', s = 'black') {
   const line = {
     x1: x1,
     y1: y1,
@@ -9,11 +9,20 @@ function drawLine(x1 = '0', y1 = '10', x2 = '1000', y2 = '10', w = '1', s = 'bla
   };
   return line;
 }
-module.exports = drawLine;
 
-// x1: '0',
-// y1: '10',
-// x2: '1000',
-// y2: '10',
-// strokeWidth: '1',
-// stroke: 'black',
+function lineArray() {
+  const lines = [];
+  const line =
+    {
+      x1: '0',
+      y1: '10',
+      x2: '1000',
+      y2: '10',
+      strokeWidth: '1',
+      stroke: 'black',
+    };
+  lines.push(line);
+  return lines;
+}
+
+module.exports = { line, lineArray };
--- src/jest/drawLine.test.js
+++ src/jest/drawLine.test.js
@@ -1,7 +1,7 @@
-const drawLine = require('./drawLine');
+const lines = require('./drawLine');
 
-test('generates an object with default key/values', () => {
-  expect(drawLine()).toEqual(
+test('generates a line object with default key/values', () => {
+  expect(lines.line()).toEqual(
     {
       x1: '0',
       y1: '10',
@@ -10,4 +10,16 @@ test('generates an object with default key/values', () => {
       strokeWidth: '1',
       stroke: 'black',
     });
+});
+
+test('pushes object into array', () => {
+  expect(lines.lineArray()).toEqual(
+    [{
+      x1: '0',
+      y1: '10',
+      x2: '1000',
+      y2: '10',
+      strokeWidth: '1',
+      stroke: 'black',
+    }]);
 });
