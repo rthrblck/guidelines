20fed49d260bc2b87f433b22fc1867cd14d8d59e
Arthur Black <hellomynameisarthurblack@gmail.com>
2019-02-19 22:13:51 -0700

    Change line() to accept object inputs and maintain default values. Change lineGroup() to include functionality of lineArray(), use variables instead of fixed values, and call line() using objects as args. Write tests to match new functionality. Delete lineArray() and associated test

--- src/jest/drawLine.js
+++ src/jest/drawLine.js
@@ -6,51 +6,41 @@ function calc(xHeight = 4, aHeight = 2, dHeight = 2, iSpace = 2, nibWidth = 3) {
 }
 
 // Dumb function. Generates an object using function args as properties.
-function line(x1 = '0', y1 = '10', x2 = '1000', y2 = '10', w = '1', s = 'black') {
+function line(inputs = {}, defaults = {x1: 0, y1: 10, x2: 1000, y2: 10, w: 1, s: 'black'}) {
+  const mergedLine = Object.assign({}, defaults, inputs)
   const line = {
-    x1: x1,
-    y1: y1,
-    x2: x2,
-    y2: y2,
-    strokeWidth: w,
-    stroke: s,
+    x1: mergedLine.x1,
+    y1: mergedLine.y1,
+    x2: mergedLine.x2,
+    y2: mergedLine.y2,
+    strokeWidth: mergedLine.w,
+    stroke: mergedLine.s,
   };
   return line;
 }
 
-// Dumb Function. Pushes object created by previous function into an array
-function lineArray() {
-  const lines = [];
-  const line =
-    {
-      x1: '0',
-      y1: '10',
-      x2: '1000',
-      y2: '10',
-      strokeWidth: '1',
-      stroke: 'black',
-    };
-  lines.push(line);
-  return lines;
-}
-
-// Smart function? Manages user inputs and uses above functions to produce array of lines.
+// Smart function? Manages user inputs and uses above functions to produce array of lines. Will eventually need to be able to name all of the arrays it's creating based on which guideline they render. Might eventually be able to hand diagonal guidelines as well, but that might be another function
 function lineGroup() {
   // Define topMargin variable to use as y initialExpression
-  const topMargin = topMargin
+  const topMargin = 10
+  const pageHeight = 1000
+  const lineSpacing = 30
+  // All user inputs need to be defined here
+  const lineArray = []
   for (
     // initialExpression: Y equals topMargin (10px as placeholder)
-    let y = 10;
+    let y = topMargin;
     // condition: As long as y ≤ pageHeight (1000px as placeholder)
-    y <= 1000;
+    y <= pageHeight;
     // incrementExpressions: Increment y by lineSpacing (30px as placeholder)
-    y += 30
+    y += lineSpacing
   )
   {
     //statement:
-      // Call calc() to determine lineSpacing
-      // Call line() to generate line
-      // Call lineArray to push line into array
+      // Call line() to generate line using current y value for y1 and y2
+    const lineX = line({y1: y, y2: y});
+    lineArray.push(lineX);
   }
+  return lineArray;
 }
-module.exports = { calc, line, lineArray };
+module.exports = { calc, line, lineGroup };
--- src/jest/drawLine.test.js
+++ src/jest/drawLine.test.js
@@ -3,29 +3,73 @@ const lines = require('./drawLine');
 test('generates a line object with default key/values', () => {
   expect(lines.line()).toEqual(
     {
-      x1: '0',
-      y1: '10',
-      x2: '1000',
-      y2: '10',
-      strokeWidth: '1',
+      x1: 0,
+      y1: 10,
+      x2: 1000,
+      y2: 10,
+      strokeWidth: 1,
       stroke: 'black',
     });
 });
 
-test('pushes object into array', () => {
-  expect(lines.lineArray()).toEqual(
-    [{
-      x1: '0',
-      y1: '10',
-      x2: '1000',
-      y2: '10',
-      strokeWidth: '1',
-      stroke: 'black',
-    }]);
-});
-
-test('calculates user inputs to determine line interval', () =>{
+test('calculates user inputs to determine line interval', () => {
   expect(lines.calc()).toEqual(
     lineSpacing = 30
   );
+})
+
+test('lineGroup with default values give me 34 lines', () => {
+  expect(lines.lineGroup().length).toEqual(34);
+})
+
+test('lineGroup with default values give me an expected first line object', () => {
+  const lineArray = lines.lineGroup()
+  expect(lineArray[0]).toEqual(
+    {
+      x1: 0,
+      y1: 10,
+      x2: 1000,
+      y2: 10,
+      strokeWidth: 1,
+      stroke: 'black',
+    });
+})
+
+test('lineGroup with default values give me an expected second line object', () => {
+  const lineArray = lines.lineGroup()
+  expect(lineArray[1]).toEqual(
+    {
+        x1: 0,
+        y1: 40,
+        x2: 1000,
+        y2: 40,
+        strokeWidth: 1,
+        stroke: 'black',
+      });
+})
+
+test('lineGroup with default values give me an expected third line object', () => {
+  const lineArray = lines.lineGroup()
+  expect(lineArray[2]).toEqual(
+    {
+        x1: 0,
+        y1: 70,
+        x2: 1000,
+        y2: 70,
+        strokeWidth: 1,
+        stroke: 'black',
+      });
+})
+
+test('lineGroup with default values give me an expected last line object', () => {
+  const lineArray = lines.lineGroup()
+  expect(lineArray[lineArray.length - 1]).toEqual(
+    {
+        x1: 0,
+        y1: 1000,
+        x2: 1000,
+        y2: 1000,
+        strokeWidth: 1,
+        stroke: 'black',
+      });
 })
