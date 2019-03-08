a0b555866ccc767bf0075b21799b9e67010a9472
Arthur Black <hellomynameisarthurblack@gmail.com>
2019-03-07 21:32:36 -0700

    Add diagLine function and use it to create new lineArray entries. Update Jest tests. Clean up sytax a little.

--- src/jest/drawLine.js
+++ src/jest/drawLine.js
@@ -1,34 +1,44 @@
-// First attempt at converting variables into object properties
+// Placeholder for React state inputs
 const state = {
+// Page dimensions
   tM: 10,
   bM: 20,
   lM: 10,
   rM: 10,
-  nW: 3,
   pH: 1000,
+// Base proportions
+  nW: 3,
   aH: 2,
   xH: 4,
   dH: 2,
   iS: 2,
+// Angled lines
+  vA: 10,
+  vS: 30,
+  pA: 30,
+  pS: 30,
 };
 
-// Dumb function. Generates a line object using function args as properties with defaults as backup.
-function line(
-  currentY,
-  defaults = {
+// Generates a line object using function args as properties with defaults as backup. Receives single Y input and inserts it into y1 and y2.
+function horizLine(currentY) {
+// Define default and input objects
+  const defaults = {
     x1: 0,
     y1: 10,
     x2: 1000,
     y2: 10,
     w: 1,
     s: 'black',
-  },
-) {
+  };
   const inputs = {
     y1: currentY,
     y2: currentY,
   };
-  const mergedLine = Object.assign({}, defaults, inputs)
+
+  // Combine default and input objects into new object
+  const mergedLine = Object.assign({}, defaults, inputs);
+
+  // Define line based on merged object
   const line = {
     x1: mergedLine.x1,
     y1: mergedLine.y1,
@@ -37,34 +47,82 @@ function line(
     strokeWidth: mergedLine.w,
     stroke: mergedLine.s,
   };
+
   return line;
 }
 
-// Smart function Manages user inputs and produces array of lines.
+// First stab at diagonal line drawing function
+function diagLine(angle, currentX) {
+  // Define default and input objects
+  const defaults = {
+    x1: 0,
+    y1: 0,
+    x2: 0,
+    y2: 1000,
+    w: 1,
+    s: 'gray',
+    r: 'rotate(0, 0, 0)',
+  };
+  const inputs = {
+    x1: currentX,
+    x2: currentX,
+    r: `rotate(${angle}, ${currentX}, 0)`,
+  };
+  // Combine default and input objects into new object
+  const mergedLine = Object.assign({}, defaults, inputs);
+  // Define line based on merged object
+  const line = {
+    x1: mergedLine.x1,
+    y1: mergedLine.y1,
+    x2: mergedLine.x2,
+    y2: mergedLine.y2,
+    strokeWidth: mergedLine.w,
+    stroke: mergedLine.s,
+    transform: mergedLine.r,
+  };
+
+  return line;
+}
+
+// Manages user inputs and produces array of lines.
 // Might be able to handle diagonal lines as well?
 
 function lineGroup(inputs = state) {
   // Calculate line spacing period from user inputs in nib widths
   const lineS = (inputs.aH + inputs.xH + inputs.dH + inputs.iS);
-  let currentY;
   const lineArray = [];
 
+  // Generates array of the 4 basic horizontal lines.
   for (
     let y = inputs.tM;
     y + inputs.aH + inputs.xH + inputs.dH <= inputs.pH;
     y += lineS
   ) {
-    const aLine = line(currentY = y);
+    const aLine = horizLine(y);
 
-    const xLine = line(currentY = y + inputs.aH);
+    const xLine = horizLine(y + inputs.aH);
 
-    const bLine = line(currentY = y + inputs.aH + inputs.xH);
+    const bLine = horizLine(y + inputs.aH + inputs.xH);
 
-    const dLine = line(currentY = y + inputs.aH + inputs.xH + inputs.dH);
+    const dLine = horizLine(y + inputs.aH + inputs.xH + inputs.dH);
 
     lineArray.push(aLine, xLine, bLine, dLine);
   }
 
+  // Generates array of diagonal verticality lines
+  for (
+    // Starting X is left margin plus the vertical spacing
+    let x = inputs.lM + inputs.vS;
+    // To cover page in diag lines, need to generate lines far past the right margin. Placholder.
+    x <= 2000;
+    // Increment by the spacing between vert lines
+    x += inputs.vS
+  ) {
+    const vLine = diagLine(inputs.vA, x);
+
+    lineArray.push(vLine);
+  }
+
   return lineArray;
 }
-module.exports = { line, lineGroup };
+module.exports = { horizLine, diagLine, lineGroup };
--- src/jest/drawLine.test.js
+++ src/jest/drawLine.test.js
@@ -1,102 +1,105 @@
 const lines = require('./drawLine');
 
-test('generates a line object with default key/values', () => {
-  expect(lines.line(y = 10)).toEqual(
-    {
-      x1: 0,
-      y1: 10,
-      x2: 1000,
-      y2: 10,
-      strokeWidth: 1,
-      stroke: 'black',
-    });
+// Horizontal line
+test('generates a horizontal line object with default key/values', () => {
+  expect(lines.horizLine(10)).toEqual({
+    x1: 0,
+    y1: 10,
+    x2: 1000,
+    y2: 10,
+    strokeWidth: 1,
+    stroke: 'black',
+  });
 });
 
-// test('calculates user inputs to determine line interval', () => {
-//   expect(lines.calc()).toEqual(
-//     lineS = 30
-//   );
-// })
+// Diagonal line
+test('generates a diagonal line object with default key/values', () => {
+  expect(lines.diagLine(20, 20)).toEqual({
+    x1: 20,
+    y1: 0,
+    x2: 20,
+    y2: 1000,
+    strokeWidth: 1,
+    stroke: 'gray',
+    transform: 'rotate(20, 20, 0)',
+  });
+});
+// LineGroup
 
-test('lineGroup with default values give me 396 lines', () => {
-  expect(lines.lineGroup().length).toEqual(396);
-})
+test('lineGroup with default values give me 462 lines', () => {
+  expect(lines.lineGroup().length).toEqual(462);
+});
 
 test('lineGroup with default values give me an expected first line object', () => {
-  const lineArray = lines.lineGroup()
-  expect(lineArray[0]).toEqual(
-    {
-      x1: 0,
-      y1: 10,
-      x2: 1000,
-      y2: 10,
-      strokeWidth: 1,
-      stroke: 'black',
-    });
-})
+  const lineArray = lines.lineGroup();
+  expect(lineArray[0]).toEqual({
+    x1: 0,
+    y1: 10,
+    x2: 1000,
+    y2: 10,
+    strokeWidth: 1,
+    stroke: 'black',
+  });
+});
 
 test('lineGroup with default values give me an expected second line object', () => {
-  const lineArray = lines.lineGroup()
-  expect(lineArray[1]).toEqual(
-    {
-        x1: 0,
-        y1: 12,
-        x2: 1000,
-        y2: 12,
-        strokeWidth: 1,
-        stroke: 'black',
-      });
-})
+  const lineArray = lines.lineGroup();
+  expect(lineArray[1]).toEqual({
+    x1: 0,
+    y1: 12,
+    x2: 1000,
+    y2: 12,
+    strokeWidth: 1,
+    stroke: 'black',
+  });
+});
 
 test('lineGroup with default values give me an expected third line object', () => {
-  const lineArray = lines.lineGroup()
-  expect(lineArray[2]).toEqual(
-    {
-        x1: 0,
-        y1: 16,
-        x2: 1000,
-        y2: 16,
-        strokeWidth: 1,
-        stroke: 'black',
-      });
-})
+  const lineArray = lines.lineGroup();
+  expect(lineArray[2]).toEqual({
+    x1: 0,
+    y1: 16,
+    x2: 1000,
+    y2: 16,
+    strokeWidth: 1,
+    stroke: 'black',
+  });
+});
 
 test('lineGroup with default values give me an expected fourth line object', () => {
-  const lineArray = lines.lineGroup()
-  expect(lineArray[3]).toEqual(
-    {
-        x1: 0,
-        y1: 18,
-        x2: 1000,
-        y2: 18,
-        strokeWidth: 1,
-        stroke: 'black',
-      });
-})
+  const lineArray = lines.lineGroup();
+  expect(lineArray[3]).toEqual({
+    x1: 0,
+    y1: 18,
+    x2: 1000,
+    y2: 18,
+    strokeWidth: 1,
+    stroke: 'black',
+  });
+});
 
 test('lineGroup with default values give me an expected fifth line object', () => {
-  const lineArray = lines.lineGroup()
-  expect(lineArray[4]).toEqual(
-    {
-        x1: 0,
-        y1: 20,
-        x2: 1000,
-        y2: 20,
-        strokeWidth: 1,
-        stroke: 'black',
-      });
-})
+  const lineArray = lines.lineGroup();
+  expect(lineArray[4]).toEqual({
+    x1: 0,
+    y1: 20,
+    x2: 1000,
+    y2: 20,
+    strokeWidth: 1,
+    stroke: 'black',
+  });
+});
 
 
 test('lineGroup with default values give me an expected last line object', () => {
-  const lineArray = lines.lineGroup()
-  expect(lineArray[lineArray.length - 1]).toEqual(
-    {
-        x1: 0,
-        y1: 998,
-        x2: 1000,
-        y2: 998,
-        strokeWidth: 1,
-        stroke: 'black',
-      });
-})
+  const lineArray = lines.lineGroup();
+  expect(lineArray[lineArray.length - 1]).toEqual({
+    x1: 1990,
+    y1: 0,
+    x2: 1990,
+    y2: 1000,
+    strokeWidth: 1,
+    stroke: 'gray',
+    transform: 'rotate(10, 1990, 0)',
+  });
+});
