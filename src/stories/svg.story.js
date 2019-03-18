827e644cdd34ea280b96585f83c0ea2aa128371b
Arthur Black <hellomynameisarthurblack@gmail.com>
2019-03-18 16:09:03 -0600

    Render an svg clipping mask based on user input margins. Overlay with rectangle of same dimensions to address the top margin being clipped in half (svg aligns stroke on center only)

--- src/stories/svg.story.js
+++ src/stories/svg.story.js
@@ -4,42 +4,42 @@ import { storiesOf } from '@storybook/react';
 
 import SVG from '../svg';
 
-const lineArray = [
-  {
-    x1: '10',
-    y1: '0',
-    x2: '10',
-    y2: '1000',
-    strokeWidth: '1',
-    stroke: 'gray',
-    transform: 'rotate(10, 10, 0)',
-  },
-  {
-    x1: '30',
-    y1: '0',
-    x2: '30',
-    y2: '1000',
-    strokeWidth: '1',
-    stroke: 'gray',
-    transform: 'rotate(10, 10, 0)',
-  },
-  {
-    x1: '0',
-    y1: '70',
-    x2: '1000',
-    y2: '70',
-    strokeWidth: '1',
-    stroke: 'black',
-  },
-  {
-    x1: '0',
-    y1: '90',
-    x2: '1000',
-    y2: '90',
-    strokeWidth: '1',
-    stroke: 'black',
-  },
-];
+// const lineArray = [
+//   {
+//     x1: '10',
+//     y1: '0',
+//     x2: '10',
+//     y2: '1000',
+//     strokeWidth: '1',
+//     stroke: 'gray',
+//     transform: 'rotate(10, 10, 0)',
+//   },
+//   {
+//     x1: '30',
+//     y1: '0',
+//     x2: '30',
+//     y2: '1000',
+//     strokeWidth: '1',
+//     stroke: 'gray',
+//     transform: 'rotate(10, 10, 0)',
+//   },
+//   {
+//     x1: '0',
+//     y1: '70',
+//     x2: '1000',
+//     y2: '70',
+//     strokeWidth: '1',
+//     stroke: 'black',
+//   },
+//   {
+//     x1: '0',
+//     y1: '90',
+//     x2: '1000',
+//     y2: '90',
+//     strokeWidth: '1',
+//     stroke: 'black',
+//   },
+// ];
 
 class MySVG extends React.Component {
   constructor(props) {
@@ -55,7 +55,5 @@ class MySVG extends React.Component {
 
 storiesOf('svg', module)
   .add('default horiz lines', () => (
-    <SVG
-      lineArray={lineArray}
-    />
+    <SVG />
   ));
--- src/svg.js
+++ src/svg.js
@@ -1,9 +1,81 @@
 import React from 'react';
 
+const state = {
+  pW: 600,
+  pH: 800,
+  lM: 10,
+  rM: 10,
+  tM: 0,
+  bM: 10,
+};
+
+const lineArray = [
+  {
+    x1: '0',
+    y1: '10',
+    x2: '1000',
+    y2: '10',
+    strokeWidth: '1',
+    stroke: 'black',
+  },
+  {
+    x1: '0',
+    y1: '30',
+    x2: '1000',
+    y2: '30',
+    strokeWidth: '1',
+    stroke: 'black',
+  },
+  {
+    x1: '0',
+    y1: '70',
+    x2: '1000',
+    y2: '70',
+    strokeWidth: '1',
+    stroke: 'black',
+  },
+  {
+    x1: '0',
+    y1: '90',
+    x2: '1000',
+    y2: '90',
+    strokeWidth: '1',
+    stroke: 'black',
+  },
+];
+
 const SVG = props => (
-  <svg width="8500" height="1100">
-    {props.lineArray.map(line =>
-      <line {...line} />)}
+  <svg width={state.pW} height={state.pH}>
+
+    <defs>
+      <clipPath id="margins">
+        <rect
+          x={state.lM}
+          y={state.tM}
+          width={state.pW - (state.lM + state.rM)}
+          height={state.pH - (state.tM + state.bM)}
+        />
+      </clipPath>
+    </defs>
+
+    <g clipPath="url(#margins)">
+      {lineArray.map(line =>
+        (<line
+          {...line}
+          stroke="red"
+        />))}
+    </g>
+
+    <rect
+      x={state.lM}
+      y={state.tM}
+      width={state.pW - (state.lM + state.rM)}
+      height={state.pH - (state.tM + state.bM)}
+      stroke="red"
+      strokeWidth="1"
+      fill="none"
+    />
+
   </svg>
 );
 
