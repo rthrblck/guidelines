201b1d748e30a4bcbff7ff967b12d105e8e3fb4b
Arthur Black <hellomynameisarthurblack@gmail.com>
2019-04-15 10:27:54 -0600

    Pass state and handleChange from story to Inputs. It frickin works.

--- src/stories/inputs.story.js
+++ src/stories/inputs.story.js
@@ -1,47 +1,38 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-// import { action } from '@storybook/addon-actions';
-// import { linkTo } from '@storybook/addon-links';
 
 import Inputs from '../inputs';
 
-class InputsApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // Fundamental lines
-      nW: 3,
-      xH: 5,
-      aH: 5,
-      dH: 5,
-      cH: 4,
-      bH: 0,
-      iS: 1,
-      // Verticality and pen angle
-      vA: 90,
-      vS: 10,
-      pA: 0,
-      pS: 10,
-      // Page size and margins
-      pH: 800,
-      pW: 1100,
-      tM: 20,
-      bM: 30,
-      lM: 20,
-      rM: 20,
-    };
+class App extends React.Component {
+  state = {
+    nW: 3,
+    xH: 5,
+    aH: 5,
+    dH: 5,
+    cH: 4,
+    bH: 0,
+    iS: 1,
+    // Verticality and pen angle
+    vA: 90,
+    vS: 10,
+    vC: 'gray',
+    pA: 0,
+    pS: 10,
+    pC: 'red',
+    // Page size and margins
+    pH: 800,
+    pW: 1100,
+    tM: 20,
+    bM: 30,
+    lM: 20,
+    rM: 20,
+  };
 
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  // handleChange uses input id as state key,
-  // so keep input ids consistent with state variable names
-
-  handleChange(event) {
+  handleChange = (event) => {
     const name = event.target.id;
     this.setState({ [name]: event.target.value });
-  }
+  };
 
   render() {
     return (
@@ -53,8 +44,7 @@ class InputsApp extends React.Component {
   }
 }
 
-
 storiesOf('inputs', module)
   .add('initial setup', () => (
-    <Inputs />
+    <App />
   ));
--- src/stories/svg.story.js
+++ src/stories/svg.story.js
@@ -3,16 +3,15 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import SVG from '../svg';
-import { svgPropsBlackLetter, appStateBlackLetter } from '../fixtures';
+import { appStateBlackLetter } from '../fixtures';
 import lineGroup from '../drawLine';
 
 const lineArray = lineGroup(appStateBlackLetter);
-const svgProps = Object.assign({ lineArray }, appStateBlackLetter);
-
+const svgBlackLetter = Object.assign({ lineArray }, appStateBlackLetter);
 
 storiesOf('svg', module)
   .add('BlackLetter', () => (
     <SVG
-      {...svgProps}
+      {...svgBlackLetter}
     />
   ));
