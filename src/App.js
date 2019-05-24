f8696a6d85dd7506de41d78c52ef54b26b2287e0
Arthur Black <hellomynameisarthurblack@gmail.com>
2019-05-23 20:49:05 -0600

    Migrate storybook infrastructure to App.js. IT VERKS

--- src/App.js
+++ src/App.js
@@ -1,18 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import Inputs from './inputs';
+
+import SVG from './svg';
+import { appStateBlackLetter } from './fixtures';
+import lineGroup from './drawLine';
+
+// This chunk needs to accept state where lineGroup lives, with a fallback default
+const lineArray = lineGroup(appStateBlackLetter);
+const svgBlackLetter = Object.assign({ lineArray }, appStateBlackLetter);
+
 class App extends Component {
+  state = {
+
+    // Basic lines
+    nW: 3,
+    xH: 5,
+    aH: 5,
+    dH: 5,
+    cH: 4,
+    bH: 0,
+    bC: 'black',
+    iS: 1,
+
+    // Verticality and pen angle
+    vA: 90,
+    vS: 10,
+    vC: 'gray',
+    pA: 0,
+    pS: 10,
+    pC: 'red',
+
+    // Page size and margins
+    pH: 800,
+    pW: 1100,
+    tM: 20,
+    bM: 30,
+    lM: 20,
+    rM: 20,
+  };
+
+  handleChange = (event) => {
+    const name = event.target.id;
+    this.setState({ [name]: event.target.value });
+  };
+
   render() {
     return (
       <div className="App">
+
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Calligraphy Guidelines Generator</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div className="inputs" style={{ float: 'left' }}>
+          <Inputs />
+        </div>
+
+      <div>
+
+        <SVG
+          className="svg"
+          {...svgBlackLetter}
+        />
+      </div>
+
       </div>
     );
   }
--- src/fixtures.js
+++ src/fixtures.js
@@ -1,94 +1,90 @@
-const svgPropsBlackLetter = {
-  lineArray: [
-    {
-      x1: '0',
-      y1: '11',
-      x2: '1000',
-      y2: '11',
-      strokeWidth: '1',
-      stroke: 'black',
-    },
-    {
-      x1: '0',
-      y1: '31',
-      x2: '1000',
-      y2: '31',
-      strokeWidth: '1',
-      stroke: 'black',
-    },
-    {
-      x1: '0',
-      y1: '71',
-      x2: '1000',
-      y2: '71',
-      strokeWidth: '1',
-      stroke: 'black',
-    },
-    {
-      x1: '0',
-      y1: '91',
-      x2: '1000',
-      y2: '91',
-      strokeWidth: '1',
-      stroke: 'black',
-    },
-  ],
-
-  pageSettings: {
-    pW: 600,
-    pH: 800,
-    lM: 10,
-    rM: 10,
-    tM: 10,
-    bM: 10,
-  },
-
-  basicLines: {
-    nW: 3,
-    xH: 5,
-    aH: 5,
-    dH: 5,
-    cH: 4,
-    bH: 0,
-    iS: 1,
-  },
-
-  angleLines: {
-    vA: 90,
-    vS: 10,
-    pA: 0,
-    pS: 10,
-  },
-
-};
+// To Do: Look up letter proportions of these styles to make sure they're accurate
 
 const appStateBlackLetter = {
 
 // Basic lines
   nW: 3,
   xH: 5,
-  aH: 5,
-  dH: 5,
-  cH: 4,
+  aH: 3,
+  dH: 3,
+  cH: 0,
   bH: 0,
   bC: 'black',
-  iS: 1,
+  iS: 3,
 
   // Angled lines
-  vA: 10,
-  vS: 100,
+  vA: 0,
+  vS: 10,
   vC: 'gray',
-  pA: 10,
-  pS: 100,
+  pA: 45,
+  pS: 10,
   pC: 'red',
 
   // Page settings
-  pH: 850,
-  pW: 1100,
+  pH: 400,
+  pW: 600,
   tM: 10,
   bM: 10,
   lM: 10,
   rM: 10,
 };
 
-export { svgPropsBlackLetter, appStateBlackLetter };
+const appStateBookhand = {
+
+// Basic lines
+  nW: 3,
+  xH: 4,
+  aH: 3,
+  dH: 3,
+  cH: 0,
+  bH: 0,
+  bC: 'black',
+  iS: 3,
+
+  // Angled lines
+  vA: 0,
+  vS: 10,
+  vC: 'gray',
+  pA: 45,
+  pS: 10,
+  pC: 'green',
+
+  // Page settings
+  pH: 400,
+  pW: 600,
+  tM: 10,
+  bM: 10,
+  lM: 10,
+  rM: 10,
+};
+
+const appStateItalic = {
+
+// Basic lines
+  nW: 3,
+  xH: 5,
+  aH: 4,
+  dH: 4,
+  cH: 0,
+  bH: 0,
+  bC: 'black',
+  iS: 3,
+
+  // Angled lines
+  vA: 0,
+  vS: 10,
+  vC: 'gray',
+  pA: 45,
+  pS: 10,
+  pC: 'red',
+
+  // Page settings
+  pH: 400,
+  pW: 600,
+  tM: 10,
+  bM: 10,
+  lM: 10,
+  rM: 10,
+};
+
+export { appStateBlackLetter, appStateBookhand, appStateItalic };
