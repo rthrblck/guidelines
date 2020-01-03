17d25263158c01acb52a9d2e40a5acbc0d753b20
Arthur Black <hellomynameisarthurblack@gmail.com>
2020-01-03 11:47:43 -0700

    Rewrite svg.js as functional component

--- src/svg.js
+++ src/svg.js
@@ -1,45 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class SVG extends React.Component {
-  render() {
-    return (
-      <svg
-        width={`${this.props.pW}mm`}
-        height={`${this.props.pH}mm`}
-        viewBox={`0 0 ${this.props.pW} ${this.props.pH}`}
-      >
+const SVG = props => (
+  <svg
+    width={`${props.pW}mm`}
+    height={`${props.pH}mm`}
+    viewBox={`0 0 ${props.pW} ${props.pH}`}
+  >
 
-        <defs>
-          <clipPath id="margins">
-            <rect
-              x={this.props.lM}
-              y={this.props.tM - 0.25}
-              width={this.props.pW - (this.props.lM + this.props.rM)}
-              height={(this.props.pH + 0.25) - (this.props.tM + this.props.bM)}
-            />
-          </clipPath>
-        </defs>
-
-        <g clipPath="url(#margins)">
-
-          {this.props.lineArray.map((line, index) =>
-            (<line key={`line${index}`} {...line} />))}
-        </g>
-
+    <defs>
+      <clipPath id="margins">
         <rect
-          x={0}
-          y={0}
-          width={this.props.pW}
-          height={this.props.pH}
-          fill="none"
-          stroke="gray"
+          x={props.lM}
+          y={props.tM - 0.25}
+          width={props.pW - (props.lM + props.rM)}
+          height={(props.pH + 0.25) - (props.tM + props.bM)}
         />
+      </clipPath>
+    </defs>
 
-      </svg>
-    );
-  }
-}
+    <g clipPath="url(#margins)">
+
+      {props.lineArray.map((line, index) =>
+        (<line key={`line${index}`} {...line} />))}
+    </g>
+
+    <rect
+      x={0}
+      y={0}
+      width={props.pW}
+      height={props.pH}
+      fill="none"
+      stroke="gray"
+    />
+
+  </svg>
+);
 
 SVG.propTypes = {
   lineArray: PropTypes.arrayOf(PropTypes.object).isRequired,
